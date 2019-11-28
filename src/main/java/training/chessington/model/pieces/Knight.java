0a3651603cc773d7d37e6a3bec933b8c1013128f
Olaf Davis <olaf.davis@softwire.com>
2019-11-28 16:03:53 +0000

    Knights added and working, but no tests for them yet!

--- src/main/java/training/chessington/model/pieces/Knight.java
+++ src/main/java/training/chessington/model/pieces/Knight.java
@@ -15,6 +15,21 @@ public class Knight extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        Integer aDiff, bDiff;
+        Coordinates to;
+        List<Move> moveList = new ArrayList<>();
+        for (aDiff = -2; aDiff <= 2; aDiff += 4) {
+            for (bDiff = -1; bDiff <= 1; bDiff += 2) {
+                to = from.plus(aDiff, bDiff);
+                if (board.isEmptyOrColour(to, colour.getOpposite())) {
+                    moveList.add(new Move(from, to));
+                }
+                to = from.plus(bDiff, aDiff);
+                if (board.isEmptyOrColour(to, colour.getOpposite())) {
+                    moveList.add(new Move(from, to));
+                }
+            }
+        }
+        return moveList;
     }
 }
