6b09998b7c3356ded86b5acefcd0f84ca264ae2a
Olaf Davis <olaf.davis@softwire.com>
2019-11-28 10:56:20 +0000

    Pawns can move forward a step

--- src/main/java/training/chessington/model/pieces/Pawn.java
+++ src/main/java/training/chessington/model/pieces/Pawn.java
@@ -5,6 +5,7 @@ import training.chessington.model.Coordinates;
 import training.chessington.model.Move;
 import training.chessington.model.PlayerColour;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +16,14 @@ public class Pawn extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        Move forwardStep;
+        List<Move> moveList = new ArrayList<Move>();
+        if (this.colour.equals(PlayerColour.WHITE)) {
+            forwardStep = (new Move(from, from.plus(-1, 0)));
+        } else {
+            forwardStep = (new Move(from, from.plus(1, 0)));
+        }
+        moveList.add(forwardStep);
+        return moveList;
     }
 }
