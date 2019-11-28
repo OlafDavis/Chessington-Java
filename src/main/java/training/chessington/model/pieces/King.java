0c2bf831090bb18a91d17b148ebee43d93b1d2a0
Olaf Davis <olaf.davis@softwire.com>
2019-11-28 16:11:27 +0000

    Kings added and working, but no tests for them yet!

--- src/main/java/training/chessington/model/pieces/King.java
+++ src/main/java/training/chessington/model/pieces/King.java
@@ -15,6 +15,17 @@ public class King extends AbstractPiece {
 
     @Override
     public List<Move> getAllowedMoves(Coordinates from, Board board) {
-        return new ArrayList<>();
+        Integer rowDiff, colDiff;
+        Coordinates to;
+        List<Move> movesList = new ArrayList<Move>();
+        for (rowDiff = -1; rowDiff <= 1; rowDiff++) {
+            for (colDiff = -1; colDiff <= 1; colDiff++) {
+                to = from.plus(rowDiff, colDiff);
+                if (board.isEmptyOrColour(to, colour.getOpposite())) {
+                    movesList.add(new Move(from, to));
+                }
+            }
+        }
+        return movesList;
     }
 }
